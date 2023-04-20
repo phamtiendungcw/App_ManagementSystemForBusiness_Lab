ef031eaeca00ce1192b76fdb043e841e4b13db0e
Phạm Tiến Dũng <56197293+phamtiendungcw@users.noreply.github.com>
2023-04-21 04:45:31 +0700

    Update fe

---
+++ App_ManagementSystemForBusiness_FE/src/app/layout/footer/footer.component.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2023 Phạm Tiến Dũng.
+ * License: MIT
+ */
+
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"],
+})
+export class FooterComponent {}
